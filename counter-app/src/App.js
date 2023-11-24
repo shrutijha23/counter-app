bd1ea2fae67113905061a769ac60f6de3e52462b
shrutijha23 <shrutijha2302@gmail.com>
2023-11-24 10:56:22 +0530

    counter-app

---
+++ counter-app/src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+   <div>
+     <div>Increment And Decrement</div>
+     <div>
+      <button>
+        -
+      </button>
+
+     <div>
+
+     </div> 
+
+     <button>
+      +
+     </button>
+
+     </div>
+
+     <button>
+      reset
+     </button>
+   </div>
+  );
+}
+
+export default App;  
